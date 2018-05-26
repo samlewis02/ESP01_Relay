f0f24d7d7ea6eee0130216f2c9ddd2f93040ca7b
samlewis02 <samlewis02@users.noreply.github.com>
2018-05-26 17:37:02 +0800

    Add files via upload

---
+++ ESP-01-MQTT-relay.ino
@@ -0,0 +1,58 @@
+/*
+ * ESP-01S
+ * 
+ * Turn relay on and off using the mainroom feed from AdafruitIO
+ * 
+ * 26/5/18
+ * 
+ */
+#include "MySSID.h"
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+// set up the feeds
+AdafruitIO_Feed *esp_01 = io.feed("mainroom"); // subscribe toggle switch input ("ON"/"OFF")
+#define RELAY_PIN 0
+
+void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW); // relay off
+  // start the serial connection
+  Serial.begin(115200);
+
+  // connect to io.adafruit.com
+  Serial.println("Connecting to Adafruit IO");
+  io.connect();
+
+  esp_01->onMessage(handleMessage);
+
+  // wait for a connection
+  while (io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+}
+
+void loop() {
+  io.run();
+}
+
+void handleMessage(AdafruitIO_Data *data) {
+
+  Serial.print("esp_01 received <- ");
+
+  if (data->toString() == "ON") {
+    Serial.println("Relay ON");
+    digitalWrite(RELAY_PIN, HIGH);
+  }
+  else if (data->toString() == "OFF") {
+    Serial.println("Relay OFF");
+    digitalWrite(RELAY_PIN, LOW);
+
+  }
+}
+
