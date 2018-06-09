beb75ae814c24e00ca552532d36ca792efd988bb
Sam Lewis <samlewis02@users.noreply.github.com>
2018-06-09 17:49:06 +0800

    Static IP and OTA

--- ESP-01-MQTT-relay.ino
+++ ESP-01-MQTT-relay.ino
@@ -1,58 +1,88 @@
 /*
- * ESP-01S
- * 
- * Turn relay on and off using the 'mainroom' feed from AdafruitIO
- * 
- * 26/5/18
- * 
- */
+* ESP-01S
+*
+* Turn relay on and off using the mainroom feed from AdafruitIO
+*
+* 26/5/18
+*
+*/
 #include "MySSID.h"
 #include "AdafruitIO_WiFi.h"
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
+
+const char* host = "esp01-relay";
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
 // set up the feeds
 AdafruitIO_Feed *esp_01 = io.feed("mainroom"); // subscribe toggle switch input ("ON"/"OFF")
 #define RELAY_PIN 0
+ESP8266WebServer httpServer(80);
+ESP8266HTTPUpdateServer httpUpdater;
+IPAddress ip(192,168,1,130);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
 
 void setup() {
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, LOW); // relay off
-  // start the serial connection
-  Serial.begin(115200);
+pinMode(RELAY_PIN, OUTPUT);
+digitalWrite(RELAY_PIN, LOW); // relay off
+// start the serial connection
+Serial.begin(115200);
+WiFi.mode(WIFI_AP_STA);
+WiFi.config(ip,gateway,subnet);
+WiFi.begin(ssid, password);
 
-  // connect to io.adafruit.com
-  Serial.println("Connecting to Adafruit IO");
-  io.connect();
+while(WiFi.waitForConnectResult() != WL_CONNECTED){
+  WiFi.begin(ssid, password);
+  Serial.println("WiFi failed, retrying.");
+}
 
-  esp_01->onMessage(handleMessage);
+Serial.println("WiFi connected");
+Serial.println("IP address: ");
+Serial.println(WiFi.localIP());
+MDNS.begin(host);
 
-  // wait for a connection
-  while (io.status() < AIO_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
+  httpUpdater.setup(&httpServer);
+httpServer.begin();
 
-  // we are connected
-  Serial.println();
-  Serial.println(io.statusText());
+MDNS.addService("http", "tcp", 80);
+Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", host);
+
+
+// connect to io.adafruit.com
+Serial.println("Connecting to Adafruit IO");
+io.connect();
+
+esp_01->onMessage(handleMessage);
+
+// wait for a connection
+while (io.status() < AIO_CONNECTED) {
+  Serial.print(".");
+  delay(500);
+}
+
+// we are connected
+Serial.println();
+Serial.println(io.statusText());
 }
 
 void loop() {
-  io.run();
+io.run();
+ httpServer.handleClient();
 }
 
 void handleMessage(AdafruitIO_Data *data) {
 
-  Serial.print("esp_01 received <- ");
+Serial.print("esp_01 received <- ");
 
-  if (data->toString() == "ON") {
-    Serial.println("Relay ON");
-    digitalWrite(RELAY_PIN, HIGH);
-  }
-  else if (data->toString() == "OFF") {
-    Serial.println("Relay OFF");
-    digitalWrite(RELAY_PIN, LOW);
-
-  }
+if (data->toString() == "ON") {
+  Serial.println("Relay ON");
+  digitalWrite(RELAY_PIN, HIGH);
 }
+else if (data->toString() == "OFF") {
+  Serial.println("Relay OFF");
+  digitalWrite(RELAY_PIN, LOW);
 
+}
+}
